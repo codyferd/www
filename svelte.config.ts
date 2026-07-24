e399acb3d91453c3fedc0a03e2ae3925a342c5cb
Luca Ferdinand <lucarappferdinand@gmail.com>
2026-07-24 11:52:15 -0400

    GitHub Pages

---
+++ svelte.config.ts
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            // Uses BASE_PATH when passed (e.g., in GitHub Actions), defaults to empty string locally
+            base: process.env.BASE_PATH || ''
+        }
+    }
+};
+
+export default config;
